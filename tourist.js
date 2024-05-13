ea5f30757e3c41451af6f076f9e7e707ef755f90
jirapapapa <158023543+jirapapapa@users.noreply.github.com>
2024-05-13 17:58:52 +0700

    Add files via upload

---
+++ tourist.js
@@ -0,0 +1,156 @@
+var tourpoint = 
+{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Wat Phra Kaew",
+          "type": "cultural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            100.49269070899186,
+            13.751523870340762
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Mon Cham ",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            98.82269017118148,
+            18.935489775241663
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "kiriwong",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            99.76004879091471,
+            8.440460707485414
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Koh Luang Waterfall",
+          "type": "natural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            98.81763593441451,
+            17.583319655437734
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Pakbia Island",
+          "type": "natural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            98.6766197711637,
+            8.11797213327516
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Phu Chi Fa Forest Park",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            100.4473699882609,
+            19.854895409190732
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Phu tab berk",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            101.10587345269369,
+            16.89721415793869
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Pulangka",
+          "type": "natural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.5100509999993,
+            19.35782097572337
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Thale Waek",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            98.81123187689616,
+            7.96075288021386
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Wat  Yang Yai",
+          "type": "cultural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            99.91109988025471,
+            8.715656685779763
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
